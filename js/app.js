e9644c3daabbd9f0e917596842c98ca84f8a8f99
刘强 <thinkgarden1990@163.com>
2016-01-20 17:26:03 +0800

    day 8 down

--- js/app.js
+++ js/app.js
@@ -1,9 +1,14 @@
 window.onload = function() {
   console.log("page loaded");
   makeCartScrollNicely();
+  var $toggle = document.querySelector(".site__right-sidebar-toggle");
+  $toggle.addEventListener("click",function() {
+    document.body.classList.toggle("js-show-right-sidebar");
+  });
 }
 
 function makeCartScrollNicely(){
   var cart = document.querySelector(".cart");
   Ps.initialize(cart);
 }
+
